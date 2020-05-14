68bc7a08aa5f1bb4eedfc2ef6382b40da1372f31
Josh Boudreau <joshuaboud@hotmail.com>
2020-05-14 15:09:55 -0300

    periodically polls

--- sensors/read_sensors.js
+++ sensors/read_sensors.js
@@ -1,33 +1,27 @@
 const output = document.getElementById("output");
 const result = document.getElementById("result");
-const button = document.getElementById("read");
 
 function sensors_run() {
     cockpit.spawn(["sensors"])
         .stream(sensors_output)
         .then(sensors_success)
         .catch(sensors_fail);
-
-    result.innerHTML = "";
-    output.innerHTML = "";
 }
+
+var interval = setInterval(sensors_run, 5000);
 
 function sensors_success() {
     result.style.color = "green";
-    result.innerHTML = "success";
 }
 
 function sensors_fail() {
     result.style.color = "red";
-    result.innerHTML = "fail";
 }
 
 function sensors_output(data) {
+    output.innerHTML = "";
     output.append(document.createTextNode(data));
 }
-
-// Connect the button to starting the "ping" process
-button.addEventListener("click", sensors_run);
 
 // Send a 'init' message.  This tells integration tests that we are ready to go
 cockpit.transport.wait(function() { });
